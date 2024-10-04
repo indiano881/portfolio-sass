de0f11d23f1bda488297ccdd290d29d0384962cd
davidebaldi <d.baldi88@gmail.com>
2024-10-04 15:15:40 +0200

    fixed hover navbar justify content and margin for buttons container

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: "Parking Time",
     subtitle: "Company's Homepage",
-    content: "A real-world project developed in collaboration with Agile and UI/UX design students. Parking Time was built with the help of three other frontend developers, and we had weekly presentations with the client. The best features? Internationalization with next-intl, cool animations, and fetching external data with Groq and Sanity!",
+    content: "A real-world project developed in collaboration with Agile and UI/UX design students. Parking Time was built with the help of three other frontend developers, and we had weekly presentations with the client. What is the best features? Internationalization with next-intl, cool animations, and fetching external data with Sanity and Groq!",
     tech: "TypeScript, Next.js, React, Tailwind, Google Recaptcha V3, Sanity, Groq, Internationalization",
     image: "parking.png",
     order: 1,
@@ -24,7 +24,7 @@ export const projects = [
     title: "Economic MUI",
     subtitle: "Dashboard",
     content: "A SPA dashboard built with Material-UI (MUI) that allows users to visualize and analyze economic data through interactive charts and graphs. It includes filtering options, real-time updates, and customizable layouts. Data is fetched from multiple APIs, and React hooks like useState() and useEffect() were extensively used.",
-    tech: "TypeScript, React, Material-UI, SPA",
+    tech: "TypeScript, React, Material-UI, Single Page Application",
     image: "economic.png",
     order: 3,
     siteLink: "https://economi-mui.netlify.app/",
@@ -34,7 +34,7 @@ export const projects = [
     title: "Hackathon",
     subtitle: "Event Homepage",
     content: "A dynamic web page where I explored animations using Framer Motion and experimented with parallax movements. I also worked with a CMS called Hygraph, which I absolutely loved! Google Recaptcha V2 was implemented, and the styling was done using Tailwind CSS.",
-    tech: "Next.js, React, Tailwind CSS, Framer Motion, Parallax",
+    tech: "Next.js, React, Tailwind CSS, Framer Motion, Parallax, Splashscreen animation, Hygraph, Google RecaptchaV2",
     image: "front.png",
     order: 4,
     siteLink: "https://front-hackathon-theta.vercel.app/",
@@ -44,7 +44,7 @@ export const projects = [
     title: "My Former Site",
     subtitle: "Portfolio",
     content: "My first attempt at creating a professional portfolio! This project dates back to March 2024, when I had only been coding for six months. Here, I began to learn the power of React and the beauty of creating a single-page application!",
-    tech: "HTML, CSS, JavaScript, React, SPA",
+    tech: "HTML, CSS, JavaScript, React, Single Page Application",
     image: "davide.png",
     order: 5,
     siteLink: "https://davide-baldi.netlify.app/",
@@ -59,5 +59,17 @@ export const projects = [
     order: 6,
     siteLink: "https://indiano881.github.io/Mastermind-5thAttempt/",
     githubLink: "https://github.com/indiano881/Mastermind-5thAttempt"
-  }
+  },
+  /*{
+    title: "Mem App",
+    subtitle: "Management Tasks App",
+    content: "This project was completed during the CSS Libraries course. We had to investigate and create a small app using Styled Components. Despite being an older technology, I really loved it and appreciated its beauty! One of the challenges I faced was storing an array in localStorage. ChatGPT couldnâ€™t help, so I had to do some research on Stack Overflow!",
+    tech: "TypeScript, React, Styled Components, LocalStorage",
+    image: "mem.png",
+    order: 7,
+    siteLink: "https://men-app.netlify.app/",
+    githubLink: "https://github.com/indiano881/mem-app",
+ }*/
 ];
+
+ 
