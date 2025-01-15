9b09bb248d7da95c9d47feee6800014864c385d0
davidebaldi <d.baldi88@gmail.com>
2025-01-15 10:44:37 +0100

    added nuxtermind as main project and change pictures

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -1,29 +1,28 @@
 export const projects = [
-  
+  {
+    title: "Nuxtermind",
+    subtitle: "My own modern intepretation of a classic Code-Breaking Puzzle Game",
+    content: "Nuxtermind is a very modern version of the mastermind/secret code game with a touch of some modern features. Players will be allowed to choose a character and release powers during the gameplay. A timer will be alwys set and online torunament mode will be availble so that it is possible to play online in real time.",
+    tech: "Vue 3, Nuxt, Supabase, Socket.io, Pug, Tailwind",
+    image: "nuxtermind1.png",
+    order: 1,
+    siteLink: "https://nuxtermind.vercel.app/",
+    githubLink: "https://nuxtermind.vercel.app/"
+  },
   {
     title: "Parking Time",
     subtitle: "Company's Homepage",
     content: "A real-world project developed in collaboration with Agile and UI/UX design students. Parking Time was built with the help of three other frontend developers, and we had weekly presentations with the client. What is the best features? Internationalization with next-intl, cool animations, and fetching external data with Sanity and Groq!",
     tech: "TypeScript, Next.js, React, Tailwind, Google Recaptcha V3, Sanity, Groq, Internationalization",
     image: "parking.png",
-    order: 1,
+    order: 2,
     siteLink: "https://parking-time.vercel.app/en",
     githubLink: "https://github.com/indiano881/Parking-time"
   },
   {
-    title: "Nuxtermind",
-    subtitle: "My own modern intepretation of a classic Code-Breaking Puzzle Game",
-    content: "Nuxtermind is a very modern version of the mastermind/secret code game with a touch of some modern features. Players will be allowed to choose a character and release powers during the gameplay. A timer will be alwys set and online torunament mode will be availble so that it is possible to play online in real time.",
-    tech: "Vue 3, Nuxt, Supabase, Socket.io, Pug, Tailwind",
-    image: "nuxtermind.png",
-    order: 2,
-    siteLink: "https://nuxtermind.vercel.app/",
-    githubLink: "https://nuxtermind.vercel.app/"
-  },
-  {
     title: "Pandorum",
     subtitle: "E-commerce",
-    content: "OBS-WORK IN PROGRESS//This is a simple next.js project in which you can log in with username Davide and explore a group of different exclusive products, fetched from an external API. With the use of useContext is possible to save the itmes in your wishlist.",
+    content: "This is a simple next.js project in which you can log in with username Davide and explore a group of different exclusive products, fetched from an external API. With the use of useContext is possible to save the itmes in your wishlist.",
     tech: "Next.js, Tailwind CSS, useContenxt, Splashscreen animation",
     image: "pandorum.png",
     order: 3,
@@ -39,8 +38,8 @@ export const projects = [
     order: 4,
     siteLink: "https://economi-mui.netlify.app/",
     githubLink: "https://github.com/indiano881/economicMUI"
-  },
-  {
+  }
+  /*{
     title: "Hackathon",
     subtitle: "Event Homepage",
     content: "A dynamic web page where I explored animations using Framer Motion and experimented with parallax movements. I also worked with a CMS called Hygraph, which I absolutely loved! Google Recaptcha V2 was implemented, and the styling was done using Tailwind CSS.",
@@ -49,7 +48,7 @@ export const projects = [
     order: 5,
     siteLink: "https://front-hackathon-theta.vercel.app/",
     githubLink: "https://github.com/indiano881/front-hackathon"
-  },
+  },*/
   
   /*{
     title: "Mem App",
